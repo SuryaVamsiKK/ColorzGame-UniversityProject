0fa9b06bd7cb100ce19c2c4f18b46ecbee3530fe
Surya Vamsi <kksvamsi@github.com>
2018-05-04 00:41:58 +0530

    Retry

---
+++ Colorz_Game/android/src/com/Colorz/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.Colorz.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.Colorz.game.ColorzCore;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ColorzCore(), config);
+	}
+}
