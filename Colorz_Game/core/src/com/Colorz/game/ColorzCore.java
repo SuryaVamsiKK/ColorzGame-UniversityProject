75ce2fd166ed13e119bc80d5e44dec3257630f3e
Surya Vamsi <kksvamsi@github.com>
2018-05-04 01:03:55 +0530

    yeah thats it

--- Colorz_Game/core/src/com/Colorz/game/ColorzCore.java
+++ Colorz_Game/core/src/com/Colorz/game/ColorzCore.java
@@ -13,6 +13,9 @@ public class ColorzCore extends ApplicationAdapter {
 
 	public static final int WIDTH = 607;
 	public static final int HEIGHT = 1080;
+
+	//public static final int WIDTH = 1080;
+	//public static final int HEIGHT = 1920;
 	public static String TextureFormate = ".jpg";
 
 	public static final String TITLE = "ColorZ";
